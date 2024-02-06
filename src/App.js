458b3f54a2cb451834c40d88c54d399346f32b8a
shiva kumar gattu <gshiva0018@gmail.com>
2024-02-07 00:11:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>  
+      <div className="App">
+      <h1 style={{margin:"50px 10px "}}>Background Color Setting</h1>
+      <input type="color" onClick={(event)=>{
+         document.body.style.background=event.target.value
+          
+        
+      }
+      
+      }/>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
